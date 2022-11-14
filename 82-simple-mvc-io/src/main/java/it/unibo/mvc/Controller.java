139b44c03fb952bf6ef98879d55d57ecefa90ded
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2022-11-14 12:24:10 +0100

    82 fatto Controller manca resto

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
-
+	
+	private File current = null;
+	
+	Controller(){
+		this("output.txt");
+	}
+	
+	Controller(String name){
+		current = new File(System.getProperty("user.home") + System.getProperty("file.separator") + name);		
+	}
+	
+	void SetFile(File f) {
+		this.current = f ;
+	}
+	
+	File GetFile(){
+		return current ;
+	}
+	
+	String GetPath() {
+		return current.getPath();
+	}
+	
+	void PrintOnFile (String s) throws IOException{
+		
+		Files.writeString(current.toPath(), s, StandardOpenOption.WRITE);
+	}
 }
